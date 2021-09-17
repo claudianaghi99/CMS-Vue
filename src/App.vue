fb9154a10e1556452fe7601d98e9b0d79724dd28
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-09-17 12:11:20 +0300

    Added Employee component with database connection

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Employee></Employee>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Employee from './components/Employeee.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Employee
+  },
+};
+
 </script>
 
 <style>
